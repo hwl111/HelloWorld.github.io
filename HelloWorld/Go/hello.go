650e5f97f2c88055edd5ed035c08c9af0e494255
Xinyao Qiu <xinyaoq3@illinois.edu>
2025-04-16 12:22:13 -0500

    feat: add go version of hello world example

---
+++ HelloWorld/Go/hello.go
@@ -0,0 +1,14 @@
+/*
+ This file is based on the official Go "Getting Started" tutorial:
+ https://go.dev/doc/tutorial/getting-started
+
+ Modified for demonstration purposes.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
