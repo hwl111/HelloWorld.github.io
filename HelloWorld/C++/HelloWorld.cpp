f7048ced8be401ba221002f3179ba63b64bd3f27
lthero <70460095+lthero-big@users.noreply.github.com>
2025-04-18 13:29:32 +0800

    Add files via upload
    Add Hello World program written in C++

---
+++ HelloWorld/C++/HelloWorld.cpp
@@ -0,0 +1,10 @@
+// 包含 iostream 库，用于输入输出功能
+// Include the iostream library for input and output functions
+#include <iostream>  
+
+int main() {
+     // 使用 std::cout 输出字符串 "Hello, World!" 到控制台，并换行 
+     // Use std::cout to output the string "Hello, World!" to the console and add a new line with std::endl
+    std::cout << "Hello, World!" << std::endl;  
+    return 0;  
+}  
